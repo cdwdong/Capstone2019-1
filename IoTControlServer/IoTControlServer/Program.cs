b0e70a101a2831af60440925f2a5dc76315a5d51
LyntheYtoo <43105711+LyntheYtoo@users.noreply.github.com>
2019-03-29 15:58:56 +0900

    Project Upload

---
+++ IoTControlServer/IoTControlServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoTControlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
