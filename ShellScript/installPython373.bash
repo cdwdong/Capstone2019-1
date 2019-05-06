1fab6d8432a9a4a4cde41e6070efa7ed6ee49cc5
LyntheYtoo <43105711+LyntheYtoo@users.noreply.github.com>
2019-05-06 19:49:40 +0900

    Update installPython373.bash

--- ShellScript/installPython373.bash
+++ ShellScript/installPython373.bash
@@ -1,15 +1,10 @@
 mkdir ~/download
 sudo apt -y update
-
 sudo apt -y install build-essential checkinstall
 sudo apt -y install libreadline-gplv2-dev libncursesw5-dev
-
 sudo apt -y install libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-
 wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
-
 tar xvf Python-3.7.3.tar.xz
 cd Python-3.7.3/
 ./configure
 sudo make altinstall
-
