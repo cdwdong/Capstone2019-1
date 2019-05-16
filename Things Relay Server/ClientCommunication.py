6591bd732fc500410e3a71a1268e714020877d8e
LyntheYtoo <cdwdong1@gmail.com>
2019-05-16 16:31:58 +0900

    Update ClientCommunication.py

--- Things Relay Server/ClientCommunication.py
+++ Things Relay Server/ClientCommunication.py
@@ -4,7 +4,7 @@ import ThingsSerial as serial
 import sys
 import asyncio
 from CommuHandler import ClientHandler
-
+'''
 async def tcp_echo_client(ip, port, message):
     reader, writer = await asyncio.open_connection(
         ip, port)
@@ -17,7 +17,7 @@ async def tcp_echo_client(ip, port, message):
 
     print('Close the connection')
     writer.close()
-
+'''
 
 #asyncio.run(tcp_echo_client('Hello World!'))
 async def tcp_echo_client2(reader, writer):
@@ -32,17 +32,16 @@ async def tcp_echo_client2(reader, writer):
     print('Close the connection')
     writer.close()
 
-"""
+async def callbackTest(reader, writer):
 
+    arduino = serial.ThingsSerial("/dev/ttyUSB0", 9600)
+
+    while True:
+        message = arduino.Serial_readline()
+        writer.write(message.encode())
+
+##############################################################
 ip = "52.78.166.156"
-port = 22
-handle = ClientHandler("52.78.166.156", 22)
-arduino = serial.ThingsSerial("/dev/ttyUSB0", 9600)
-
-while True:
-    message = arduino.Serial_readline();
-    
-
-    if message:
-        asyncio.run(tcp_echo_client(ip, port, message.decode("utf-8")))
-"""
+port = 8888
+handle = ClientHandler(ip, port)
+asyncio.run(handle.start(callbackTest))
