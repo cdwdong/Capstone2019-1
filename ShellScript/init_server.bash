8916c93605079fc311a642b893ed7b3d5609c45b
LyntheYtoo <43105711+LyntheYtoo@users.noreply.github.com>
2019-05-21 14:17:43 +0900

    Create init_server.bash

---
+++ ShellScript/init_server.bash
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:ubuntu-ko
+
+sudo apt-get update
+sudo apt-get install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
+sudo apt-get install python3-pip python3.7-dev
+sudo apt-get install python3.7
