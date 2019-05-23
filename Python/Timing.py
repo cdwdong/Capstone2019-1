cba8391ad5e2a28ad4a7a05107523f90dced7db4
LyntheYtoo <43105711+LyntheYtoo@users.noreply.github.com>
2019-05-23 22:20:55 +0900

    Update Timing.py

--- Python/Timing.py
+++ Python/Timing.py
@@ -1,8 +1,8 @@
 import enum
 
 class Timing(enum.Enum):
-    DEFAULT = 0
-    SEND_ID = 1
-    SEND_CODE = 2
-    SEND_DATA = 3
+    ETC = -1    # 아직 여기에 없는 상태일경우 이것을 쓴다
+    SEND_ID = 1     # ID 정보 주고 받을때 사용
+    SEND_CODE = 2   # 스크립트 주고 받을때 사용
+    SEND_DATA = 3   # 스택 데이터 주고 받을때 사용
 
