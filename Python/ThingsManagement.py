8379ac8219e608a641baf1a9dd718ab8a6f55172
jimin <thesuffering@naver.com>
2019-05-23 22:06:55 +0900

    v190523 아두이노 각자 뭐하는 놈인지 파악 후 센서데이터 파밍
    1. 뭐하는 놈인가 파악
    
    # 역활에 따라서
    2. 센서데이터 파밍 후 메시지 제작 후 버퍼에 쓰기
    or
    3. 상황처리테이블에 따라 팬 속도 조절

--- Python/ThingsManagement.py
+++ Python/ThingsManagement.py
@@ -1,61 +1,127 @@
 """
 2019-05-04
 2019-05-19
+2019-05-23
 
-각종 Things들 관리하기
-
-여기서 통신하기
+1. 라즈베리파이에 연결된 아두이노들로부터 센서값 파밍하기
+2. 통신용 전역변수에 넣기
 
 작성자: 서지민
 
 """
-from test import ThingsSerial as ts
+import ThingsSerial as ts
+import sys
+import glob
+import serial
+from datetime import datetime
 
+#통신용 전역변수
+sensor_data_list = []
+things_pointer = 0
+message_flag = 3 #추후 변경 일단 암거나함
 
 class ThingsMangement(ts.ThingsSerial):
-    #사전
-    things_list = []
+    # 사전
     things_pointer = 0
+    things_list = []
+    sensor_data_list = []
 
-    # 생성자
+    # 생성자 장치들 파악 후 등록
     def __init__(self):
-        pass
+        COM_port_list = self.serial_ports()
+
+        for com in COM_port_list:
+            things = ts.ThingsSerial(self.things_pointer, com, 9600)
+            self.add_things(things)
 
     # 장치 등록 ThingsMain 객체를 추가하기
     def add_things(self, source):
         self.things_list.append(source)
         self.things_pointer = self.things_pointer + 1
 
-    # 해당 장치 시리얼 읽기 그리고 라운드 로빈 스케줄링
+    def sensor_scheduling(self,things):
+        data = things.serial_readline()
+
+        if data:
+            date = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+            print(date)
+            print("things ID: ", things.id)
+            print("Things Role: ", things.role)
+            print("things COM: ", things.port_number)
+            print("things baudrate: ", things.baudrate)
+            print("things Data: ", data)
+
+            ######################### 여기서 최종적으로 버퍼에다 데이터 쓰기
+            message = str(message_flag) + "," + date + "," + str(things.id) + "," + "data"
+            sensor_data_list.append(message)
+
+    #상황처리테이블에 따라 팬속도 조절 동적/정적 가능
+    def fan_scheduling(self,things):
+        # serial_write(things_number, pwm 속도):
+        pass
+
+    # 각 장치들 라운드 로빈 스케줄링
     def serial_scheduling(self):
         while True:
+
+            # 역활 구분하여 스케줄링
             for things in self.things_list:
+                #print("this role is ",things.role)
 
-                data = things.serial_readline()
+                if things.role == "dust sensor":
+                    self.sensor_scheduling(things)
 
-                if data:
-                    print("things ID: ", things.id)
-                    print("things Name: ", things.name)
-                    print("things Category: ", things.category)
-                    print("things Upper_Category: ", things.upper_category)
-                    print("things COM: ", things.port_number)
-                    print("things baudrate: ", things.baudrate)
-                    print("things Data: ", data)
+                elif things.role == "fan":
+                    #상황처리테이블
+                    self.fan_scheduling(things)
 
     # 특정 장치에 대하여 값 보내기
     def serial_write(self, things_number, data):
         self.things_list[things_number].serial_readline(data)
-  
+
+    # 가용 COM포트번호 가져오기
+    def serial_ports(self):
+        """ Lists serial port names
+
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
+
+
 ######################          클래스 끝          #########################################
 
+# 관리하는 객체 생성
+manager = ThingsMangement()
+things_pointer = manager.things_pointer #연결된 아두이노 합계
 
-#테스트 예제코드
-"""
-dust_sensor = ts.ThingsSerial(0, "arduino dust_sensor", 2, 1, "COM4", 9600) # 실험용 Things 하나 생성
+manager.serial_scheduling() #스케줄링하기
 
-manager = ThingsMangement(); # 관리하는 객체 생성
 
-manager.add_things(dust_sensor) # 관리하는 객체에다 Things 넣기
 
-manager.serial_scheduling() # 데이터 수집 시작
-"""
+
+
+
+
+
+
--- Python/ThingsSerial.py
+++ Python/ThingsSerial.py
@@ -1,14 +1,15 @@
 """
 2019-05-04
+2019-05-23
 
 아두이노 시리얼 읽기
+아두이노가 하는 소리를 듣고 뭐하는 놈인지 파악하기
 
 작성자: 서지민
 
 """
 import lib.ThingsInfo as info
 import serial
-
 
 class ThingsSerial(info.ThingsInfo):
 
@@ -25,29 +26,50 @@ class ThingsSerial(info.ThingsInfo):
     #buffer pointer
     buffer_pointer = 0;
 
-    # 시리얼 객체를 생성
-    def __init__(self, id, name, category, upper_category, port_number, baudrate):
+    role = "unknown"
 
-        super().__init__(id, name, category, upper_category)
+    # 시리얼 객체를 생성
+    def __init__(self, id, port_number, baudrate):
+
+        super().__init__(id)
 
         self.port_number = port_number
         self.baudrate = baudrate
 
         self.things_serial = serial.Serial(port_number, baudrate)
 
+        #누구인지 들어보기
+        while self.role is "unknown":
+            if self.things_serial.readable():
+                res = self.things_serial.readline()
+
+                res_str = res.decode("utf-8")
+
+                print("Raw >" + res_str)
+
+                data_list = res_str.split(',')
+
+                self.role = data_list[0]
+
+                print("This things role is " + self.role)
+
+                #self.things_serial.write(1)
+
     def serial_readline(self):
         if self.things_serial.readable():
             res = self.things_serial.readline()
             res_str = res.decode("utf-8")
 
+            data_list = res_str.split(',')
+            data = data_list[1]
             #일단 string로 디코딩한다.
             #int로 변환할 필요없다. 어차피 시리얼할 때 다시 string으로 변환해야 한다.
 
-            print("Recieve Serial Data > ", res_str)
-            self.Que_buffer.append(res_str)
+            print("Recieve Serial Data > ", data)
+            self.Que_buffer.append(data)
             self.buffer_pointer = self.buffer_pointer + 1
 
-        return res
+        return data
 
     def serial_writeline(self, pwm):
         pwm_str = str(pwm).encode("utf-8")
@@ -62,7 +84,7 @@ class ThingsSerial(info.ThingsInfo):
 
 #테스트 예제 코드
 """
-begin = ThingsSerial(0, "arduino dust_sensor", 2, 1, "COM4", 9600)
+begin = ThingsSerial(0, "COM4", 9600)
 
 while True:
     begin.serial_readline()
