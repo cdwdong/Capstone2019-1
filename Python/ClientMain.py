c5ebfd64e37d226db83c10d3aad8c607899e7760
LyntheYtoo <43105711+LyntheYtoo@users.noreply.github.com>
2019-05-26 15:57:28 +0900

    Update ClientMain.py

--- Python/ClientMain.py
+++ Python/ClientMain.py
@@ -1,5 +1,6 @@
 import lib.CommuHandler as CommuHandler
 import asyncio
+import Timing
 
 def executeCode(code):
     code_obj = compile(code, '<string>', 'exec')
@@ -10,6 +11,16 @@ async def eventHandle(reader, writer):
     executeCode(code.decode())
     writer.close()
     await writer.wait_closed()
+
+def actionSelect(flag):
+    if flag == Timing.Timing.SEND_ID:
+        pass
+    elif flag == Timing.Timing.SEND_CODE:
+        pass
+    elif flag == Timing.Timing.SEND_DATA:
+        pass
+    elif flag == Timing.Timing.ETC:
+        pass
 
 client = CommuHandler.ClientHandler('52.78.166.156', 8888)
 asyncio.run(client.start(eventHandle))
