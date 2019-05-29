066f818b435456431d50cb39c6fd01ed9c6adfbd
Reynion <43945749+Reynion@users.noreply.github.com>
2019-05-29 21:58:14 +0900

    Add files via upload
    1

---
+++ GwtD/js/dash-charts.js
@@ -0,0 +1,87 @@
+﻿/*** 1 ***/
+
+	$(document).ready(function() {
+		info = new Highcharts.Chart({
+			chart: {
+				renderTo: 'load',
+				margin: [0, 0, 0, 0],
+				backgroundColor: null,
+                plotBackgroundColor: 'none',
+							
+			},
+			
+			title: {
+				text: null
+			},
+
+			tooltip: {
+				formatter: function() { 
+					return this.point.name +': '+ this.y +' %';
+						
+				} 	
+			},
+		    series: [
+				{
+				borderWidth: 2,
+				borderColor: '#F1F3EB',
+				shadow: false,	
+				type: 'pie',
+				name: 'speed',
+				innerSize: '65%',
+				data: [
+					{ name: '팬 로드', y: 65.0, color: '#B2EBF4' },
+					{ name: '휴식', y: 35.0, color: '#3d3d3d' }
+				],
+				dataLabels: {
+					enabled: false,
+					color: '#000000',
+					connectorColor: '#000000'
+				}
+			}]
+		});
+		
+	});
+
+/*** 2 ***/
+
+	$(document).ready(function() {
+		info = new Highcharts.Chart({
+			chart: {
+				renderTo: 'space',
+				margin: [0, 0, 0, 0],
+				backgroundColor: null,
+                plotBackgroundColor: 'none',
+							
+			},
+			
+			title: {
+				text: null
+			},
+
+			tooltip: {
+				formatter: function() { 
+					return this.point.name +': '+ this.y +' %';
+						
+				} 	
+			},
+		    series: [
+				{
+				borderWidth: 2,
+				borderColor: '#F1F3EB',
+				shadow: false,	
+				type: 'pie',
+				name: 'life',
+				innerSize: '65%',
+				data: [
+					{ name: '필터 수명', y: 98.0, color: '#fa1d2d' },
+					{ name: '사용', y: 2.0, color: '#3d3d3d' }
+				],
+				dataLabels: {
+					enabled: false,
+					color: '#000000',
+					connectorColor: '#000000'
+				}
+			}]
+		});
+		
+	});
