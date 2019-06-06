a872d5a106a0ec0aa2c51da99a14f1a795fa00c1
LyntheYtoo <43105711+LyntheYtoo@users.noreply.github.com>
2019-06-06 21:36:21 +0900

    Update ServerMain.py

--- Python/ServerMain.py
+++ Python/ServerMain.py
@@ -1,99 +1,40 @@
-import net.CommuHandler as CommuHandler
 import asyncio
-from Timing import *
-from net.DataProtocol import *
-import datetime
-import logging
-import logging.config
-import pymongo
+import inspect
+
+class CommuHandler:
+    def __init__(self):
+        pass
+
+    async def start(self):
+        pass
+
+    async def handle(self, reader, writer):
+        pass
 
 
-def openCode():
-    with open("./script/Remote-Script.py", 'rt', encoding='UTF8') as f:
-        return f.read()
+# 서버 생성 클래스
+class ServerHandler(CommuHandler):
+    def __init__(self, ipaddr, port):
+        CommuHandler.__init__(self)
+        self.ipaddr, self.port = ipaddr, port
 
-def writeSensorData(dataframe):
-    if dataframe.msgFlag == Timing.SEND_DATA.value:
-        logger.info(f"DB에 추가 : {dataframe.data}")
-        collection.insert(dataframe.data)
+# asyncio.run(객체명.start(callback)) 형식으로 사용
+    async def start(self, callback=None, **kwargs):
+        server = await asyncio.start_server(callback, host=self.ipaddr, port=self.port, **kwargs)
 
-
-async def eventHandle(reader, writer):
-    
-    flag = Timing.NONE.value
-
-    while flag != Timing.ERROR.value:
-        logger.debug("메시지 수신준비")
-        msg = await reader.readline()
-        date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-        if reader.at_eof():
-            logger.error("!! EOF Message Received !!")
-            break
-        try:
-            msg = msg.decode()
-            logger.debug(f"Message Received : {msg}")
-            dicmsg = DataProtocol().changeJsonToDic(msg)
-            flag = dicmsg["msgFlag"]
-            logger.debug(f"Message Flag : {flag}")
-        except ValueError:
-            logger.error(f"ERROR! WRONG MESSAGE TYPE")
-            flag = Timing.ERROR.value
-
-        if flag == Timing.NONE.value:
-            logger.warning("DATA TYPE IS Timing.NONE")
-
-        ##객체에 저장후 플래그 변환
-        elif flag == Timing.SEND_ID.value:
-            logger.debug("Timing.SEND_ID 루틴 시작")
-            logger.debug("DataProtocol_ID 객체 생성")
-            proto_id = DataProtocol_ID()
-            proto_id.takeJson(msg)
-
-            ##코드를 파일에서 읽고 객체에 담아 json으로 보내주기
-            code = openCode()
-            if code is None:
-                logger.error("code is None")
-                flag = Timing.ERROR.value
-
-            logger.debug("DataProtocol_CODE 객체 생성")
-            proto_code = DataProtocol_CODE()
-            proto_code.code = code
-            proto_code.date = date
-            proto_code.msgFlag = Timing.SEND_CODE.value
-
-            jsonmsg = proto_code.getJson() + '\n'
-
-            logger.debug(f"Message Send : {jsonmsg}")
-            writer.write(jsonmsg.encode())
-            await writer.drain()
-
-        ## 계속 반복하며 데이터객체를 읽어서 파일에 기록
-        elif flag == Timing.SEND_DATA.value:
-            logger.debug("DataProtocol_DATA 객체 생성")
-            proto_data = DataProtocol_DATA()
-            proto_data.takeJson(msg)
-            logger.debug(f"데이터 기록 : {proto_data.data}")
-            writeSensorData(proto_data)
-
-        elif flag == Timing.EXIT.value:
-            logger.debug("Timing.EXIT.value 받음 종료 루틴 시작")
-            break
-
-    writer.close()
-    await writer.wait_closed()
-    logger.info("소켓 종료됨")
+        async with server:
+            await server.serve_forever()
 
 
-logging.config.fileConfig('conf/logging.conf')
-logger = logging.getLogger()
+# 클라이언트 생성 클래스
+class ClientHandler(CommuHandler):
+    def __init__(self, ipaddr, port):
+        CommuHandler.__init__(self)
+        self.ipaddr, self.port = ipaddr, port
 
-logger.info("db 연결")
-mongo = pymongo.MongoClient('127.0.0.1', 27017)
-db = mongo.get_database('db_capstone2019')
-collection = db.get_collection('sensor_data')
-
-logger.info("서버 시작")
-server = CommuHandler.ServerHandler('172.26.3.162', 8888)
-asyncio.run(server.start(eventHandle))
-
+    async def start(self, callback, **kwargs):
+        reader, writer = await asyncio.open_connection(host=self.ipaddr, port=self.port, **kwargs)
+        if inspect.iscoroutinefunction(callback):
+            await callback(reader, writer)
+        else:
+            callback(reader, writer)
